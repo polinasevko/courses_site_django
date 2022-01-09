a043218b96e02e0e549f6830ab5be810df9b6fe6
Polina Sevko <polinasevko24@gmail.com>
2022-01-09 13:15:04 +0300

    Delete admin panel

--- course/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Course, Lecture, Hometask, Homework, Comment
-
-
-admin.site.register(Course)
-admin.site.register(Lecture)
-admin.site.register(Hometask)
-admin.site.register(Homework)
-admin.site.register(Comment)
